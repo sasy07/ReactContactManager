1750881ec939e260e8f61c71f5959c33361297d7
Sasy07 <sasan.ghafooryfard@gmail.com>
2022-05-17 23:38:26 +0430

    install bootstrap & font-awesome

---
+++ contact-manager-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h3>اپ مدیریت مخاطبین </h3>
+            <span className="fa fa-user"></span>
+            <button className="btn btn-outline-danger"><i className="fas fa-address-book"></i> دکمه</button>
+        </div>
+    );
+}
+
+export default App;
