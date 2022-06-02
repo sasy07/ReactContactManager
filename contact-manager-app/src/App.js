0f280dd7a28a19d9bb89e597b6ea5afc52e2f90b
Sasy07 <sasan.ghafooryfard@gmail.com>
2022-06-03 02:53:19 +0430

    Routing

--- contact-manager-app/src/App.js
+++ contact-manager-app/src/App.js
@@ -1,27 +1,24 @@
-import './App.css';
-import {
-    Contacts,
-    Navbar,
-    AddContact,
-    Contact,
-    EditContact,
-    ViewContact,
-    SearchContact,
-    Spinner
-} from "./components";
-import {useState} from "react";
+import "./App.css";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { AddContact, Contacts, EditContact, Navbar, ViewContact } from "./components";
+import { useState } from "react";
 
 const App = () => {
+  const [getContacts, setContacts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const [getContacts, setContacts] = useState([]);
-    const [loading, setLoading] = useState(false);
-
-    return (
-        <div className="App">
-            <Navbar/>
-            <Contacts contacts={getContacts} loading={loading}/>
-        </div>
-    );
-}
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/contacts" />} />
+        <Route path="/contacts" element={<Contacts  contacts={getContacts} loading={loading}/>} />
+        <Route path="/contacts/add" element={<AddContact />} />
+        <Route path="/contacts/:contactId" element={<ViewContact/>}/>
+        <Route path="/contacts/edit/:contactId" element={<EditContact/>}/>
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
